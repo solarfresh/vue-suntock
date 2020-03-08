a4920d232cf922abd802e8f60218237a78e7e770
黃上瑜 <shangyuhuang@gmail.com>
2020-03-09 00:47:41 +0800

    install vue-router and add menus on app-bar

---
+++ src/components/AppBar/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dense
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-toolbar-title>Suntock</v-toolbar-title>
+    </div>
+
+    <div class="d-flex align-center">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <!-- <v-btn text slot="activator"> -->
+          <v-btn text v-on="on">
+            <!-- <v-icon left>expand_more</v-icon> -->
+            <span>盤後資訊</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <!-- v-list-tile is changed to v-list-item -->
+          <v-list-item v-for="info in afterHours" :key="info.text" router :to="info.route">
+            <v-list-item-title>{{ info.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+
+    <!-- <v-spacer></v-spacer> -->
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+  data: () => ({
+    afterHours: [
+      { text: '基本面', route: '/'},
+      { text: '財務面', route: '/'},
+    ]
+  }),
+}
+</script>
---
+++ src/plugins/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', name: 'HelloWorld', component: HelloWorld }
+  ]
+});
