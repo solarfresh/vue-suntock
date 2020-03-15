19db1e09419d3240f7ba96ea20055038726bb842
黃上瑜 <shangyuhuang@gmail.com>
2020-03-15 12:39:09 +0800

    chartjs is installed

---
+++ src/atoms/Chart.vue
@@ -0,0 +1,40 @@
+<template>
+  <canvas ref="chart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js'
+
+export default {
+  name: 'Chart',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    data: {
+      type: Object,
+      required: true
+    },
+    options: {
+      type: Object,
+      required: false
+    }
+  },
+  data () {
+    return {
+      chart: null
+    }
+  },
+  mounted () {
+    if (this.$data._chart) this.$data._chart.destroy();
+
+    this.$data.chart = new Chart(
+      this.$refs['chart'].getContext('2d'), {
+      type: this.type,
+      data: this.data,
+      options: this.options
+    });
+  }
+}
+</script>
